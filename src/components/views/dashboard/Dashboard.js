5e7a0100ad0e008c8b25a02160adaf51e933938d
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2020-08-16 00:48:47 +0100

    upload avatar,update profile/auth

--- src/components/views/dashboard/Dashboard.js
+++ src/components/views/dashboard/Dashboard.js
@@ -1,24 +1,75 @@
 import React, { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { globalAuthState } from "../../../index";
-import { getUserDocument } from "../../../firebase";
+import { getUserDocument, uploadAvatar } from "../../../firebase";
 const Dashboard = () => {
   const [currentUser, setCurrentUser] = useRecoilState(globalAuthState);
-  const [localState, setLocalState] = useState({});
+  const [localState, setLocalState] = useState({
+    isCurrentUserLoaded: false,
+    avatarFile: null,
+    avatarPreviewUrl: null,
+  });
+  const { avatarFile, avatarPreviewUrl } = localState;
   const { userId } = currentUser;
   useEffect(() => {
     getUserDocument(userId).then((res) => {
-      setCurrentUser({ ...currentUser, profile: res });
-      console.log("get user profile", res);
+      setCurrentUser({ ...currentUser, ...res });
     });
-    console.log("testing");
   }, []);
-  const { email, name } = currentUser.profile || {};
+  console.log("get user profile", currentUser);
+  const { email, name, isCurrentUserLoaded } = currentUser.profile || {};
+  const fileChangeHandle = (e) => {
+    const file = e.target.files[0];
+    console.log("file", file);
+    var reader = new FileReader();
+    reader.onloadend = function () {
+      var dataURL = reader.result;
+
+      setLocalState({
+        ...localState,
+        avatarFile: file,
+        avatarPreviewUrl: dataURL,
+      });
+    };
+    reader.readAsDataURL(file); //for displaying review upload image
+    console.log(localState);
+  };
+  const submitHandle = (e) => {
+    e.preventDefault();
+    console.log("submit");
+    uploadAvatar(currentUser, avatarFile);
+  };
+  const displayImg = () => {
+    let imgUrl;
+    if (currentUser.photoURL === undefined && avatarPreviewUrl === null) {
+      imgUrl =
+        "https://www.pngkit.com/png/detail/281-2812821_user-account-management-logo-user-icon-png.png";
+    } else if (currentUser.photoURL && !avatarPreviewUrl) {
+      imgUrl = currentUser.photoURL;
+    } else {
+      imgUrl = avatarPreviewUrl;
+    }
+    return imgUrl;
+  };
   return (
     <div>
       <h1>Dashboard here</h1>
+      <figure style={{ width: "200px", height: "200px" }}>
+        <img style={{ width: "100%" }} alt="avatar" src={displayImg()} />
+      </figure>
+
       <h2>{name}</h2>
       <h3>{email}</h3>
+      <form onSubmit={submitHandle}>
+        <input
+          type="file"
+          onChange={fileChangeHandle}
+          name="avatar"
+          accept={"image/png,image/jpg"}
+        />
+        <button>Save</button>
+      </form>
+
       <div>
         <h2>My list</h2>
         <button onClick={() => alert("ADD MOVIE")}>Add Movie</button>
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+import "firebase/storage";
 import { firebaseConfig } from "./firebaseConfig";
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -8,6 +9,8 @@ firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
 
 export const firestore = firebase.firestore();
+
+export const storageRef = firebase.storage().ref();
 
 export const getUserDocument = async (uid) => {
   if (!uid) return null;
@@ -45,4 +48,46 @@ export const generateUserDocument = async (user, additionalData) => {
     }
   }
   return getUserDocument(user.uid);
+};
+
+export const uploadAvatar = async (user, file) => {
+  if (!user) return;
+
+  let fileExtension;
+  if (file.type === "image/jpeg") {
+    fileExtension = "jpg";
+  }
+  if (file.type === "image/png") {
+    fileExtension = "png";
+  }
+
+  const imgRef = storageRef.child(
+    `${user.userId}/avatar/${user.name}.${fileExtension}`
+  );
+
+  const userRef = firestore.doc(`users/${user.userId}`);
+
+  const snapshot = await userRef.get();
+
+  if (snapshot.exists) {
+    try {
+      const imgSnapshot = await imgRef.put(file); //upload image to storage
+      const imgUrl = await imgSnapshot.ref.getDownloadURL();
+      console.log("imgSnapshot", imgUrl);
+      await userRef.update({
+        photoURL: imgUrl,
+      }); //update profile in database
+      console.log("update avatar to db");
+
+      await auth.currentUser.updateProfile({
+        photoURL: imgUrl,
+      }); //update profile in auth
+      console.log("update avatar to user auth");
+      console.log("upload avatar successfully");
+    } catch (error) {
+      return { status: 400, errorMsg: error };
+    }
+  } else {
+    console.log("could not find snapshot");
+  }
 };
